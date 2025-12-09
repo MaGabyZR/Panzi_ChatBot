8226bf666a5100f3be5686af90a8e8b6d054a70e
Gabriela Rubio <gabyrubioa@gmail.com>
2025-12-09 11:21:43 -0600

    Building th chat API.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
 
 //Streamline our environment variable.
 dotenv.config();
 
+//Create a new instance of OpenAI with our API Key.
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
 //Define a route and a route handler.
 //Route
 const app = express();
+//Return a middleware function that gets executed before passing the request to the request handler. If you do not pass it, the prompt request body is gping to be undefined.
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 //Route handler.
@@ -19,6 +27,20 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello Panzi!' });
 });
 
+//Define a new endpoint for receiving prompts from the user.
+app.post('/api/chat', async (req: Request, res: Response) => {
+   //1.grab the user´s prompt from the request, using destructuring. Make sure you passed the middleware function first. (Up)
+   const { prompt } = req.body;
+   //2.Send to OpenAI
+   const response = await client.responses.create({
+      model: 'gpt-4o-mini',
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 100,
+   });
+   //3. Return a json object to the client.
+   res.json({ message: response.output_text });
+});
 //Start the webserver
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
