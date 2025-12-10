66efe685e24bb1429565227f0c9ba09230268cc9
Gabriela Rubio <gabyrubioa@gmail.com>
2025-12-10 16:51:22 -0600

    Refactoring: creating a chat service.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,17 +1,11 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
 import z from 'zod';
-import { conversationRepository } from './repositories/conversation.repository';
+import { chatService } from './services/chat.service';
 
 //Streamline our environment variable.
 dotenv.config();
-
-//Create a new instance of OpenAI with our API Key.
-const client = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-});
 
 //Define a route and a route handler.
 //Route
@@ -47,25 +41,13 @@ app.post('/api/chat', async (req: Request, res: Response) => {
       res.status(400).json(parseResult.error.format());
       return;
    }
-
    //Handle errors.
    try {
       //1.grab the user´s prompt from the request, using destructuring. Make sure you passed the middleware function first. (Up) Ang get the conversationId from the body of the request.
       const { prompt, conversationId } = req.body;
-      //2.Send to OpenAI
-      const response = await client.responses.create({
-         model: 'gpt-4o-mini',
-         input: prompt,
-         temperature: 0.2,
-         max_output_tokens: 100,
-         previous_response_id:
-            conversationRepository.getLastResponseId(conversationId),
-      });
-
-      conversationRepository.setLastResponseId(conversationId, response.id);
-
+      const response = await chatService.sendMessage(prompt, conversationId);
       //3. Return a json object to the client.
-      res.json({ message: response.output_text });
+      res.json({ message: response.message });
    } catch (error) {
       res.status(500).json({ error: 'Failed to generate a response.' });
    }
